9627aabd0dd5a9b39db6567f2de6ad575ed96b87
ManasaChinta <81618222+ManasaChinta@users.noreply.github.com>
2021-04-01 10:04:11 +0530

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,11 @@
+public class exercise
+{
+private static final double radius=5.2;
+public static void main(String args[])
+{
+double perimeter=2*Math.PI*radius;
+double area=Math.PI*radius*radius;
+System.out.println("Perimeter is="+perimeter);
+System.out.println("Area is="+area);
+}
+}
