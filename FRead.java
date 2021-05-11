97a12410804e583a7008bfd16b71a2e7e6dacacb
ManasaChinta <81618222+ManasaChinta@users.noreply.github.com>
2021-05-11 10:07:58 +0530

    Add files via upload

---
+++ FRead.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class FRead
+
+{
+
+public static void main(String a[])throws IOException,FileN
+
+{
+
+InputStream is=new FileInputStream("FRead.java");
+
+int size=is.available();
+
+for(int i=0;i<size;i++)
+
+System.out.print((char)is.read());
+
+is.close();
+
+}
+
+}
